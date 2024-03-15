89f8f24f765e59ce8a702730c7458da1c7fd9ac8
xztnb <53068693+xztnb@users.noreply.github.com>
2024-03-15 06:49:25 +0000

    Update

--- Hello.py
+++ Hello.py
@@ -1,51 +1,123 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
 
-LOGGER = get_logger(__name__)
+# 1. Loading the dataset
+df = pd.read_excel("data/table.xlsx")
+
+# 2. Preprocessing the dataset
+df["Bionic prototype"] = df["Bionic prototype"].str.strip()
+df["Materials"] = df["Materials"].str.split(";").apply(lambda x: [material.strip() for material in x])
+df["Method"] = df["Method"].str.split(";").apply(lambda x: [method.strip() for method in x])
+df["Multifunction"] = df["Multifunction"].str.split(";").apply(lambda x: [multifunction.strip() for multifunction in x])
+
+# 3. Saving the processed dataset
+# df.to_excel("data/filtered_table.xlsx", index=False)
+
+# 4. Extracting a unique list for each column
+bionic_prototype_list = df["Bionic prototype"].unique()
+method_list = df["Method"].explode().unique()
+multifunction_list = df["Multifunction"].explode().unique()
+bionic_prototype_list.sort()
+method_list.sort()
+multifunction_list.sort()
+
+ordered_multifunction_list = ["Antifogging", "Self-cleaning", "Antireflective", "Antibacterial", "Anti-icing",
+                              "Antiwetting", "Large FOV", "Fast motion detection", "Structural color",
+                              "Droplet directional migration", "Anti-drag", "Water collection",
+                              "Self-propelled actuator"]
+other_multifunction_list = [x for x in multifunction_list if x not in ordered_multifunction_list]
+ordered_multifunction_list.extend(other_multifunction_list)
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+def show_res_link(row_idx):
+    res_link = df.iloc[row_idx]['Res link']
+    st.sidebar.info(f'Resource Link: {res_link}')
+
+
+def show_bionic_prototype_content(row_idx):
+    bionic_prototype = df.iloc[row_idx]['Bionic prototype']
+    st.sidebar.info(f'Bionic prototype is: {bionic_prototype}')
+
+
+with st.sidebar:
+    st.slider(
+        label="Search results limit",
+        min_value=1,
+        max_value=50,
+        value=20,
+        step=1,
+        key="limit",
+        help="Limit the number of search results",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+    st.multiselect(
+        label="Display columns",
+        options=["Bionic prototype", "Multifunction", "Method", "Materials", "Res link"],
+        default=["Bionic prototype", "Multifunction", "Method", "Materials", "Res link"],
+        help="Select columns to display in the search results",
+        key="display_columns",
     )
 
+st.title("Bionic Path Selection")
 
-if __name__ == "__main__":
-    run()
+st.multiselect(
+    label="Multifunction",
+    options=ordered_multifunction_list,
+    default=[],
+    help="Select the multifunction to display in the search results",
+    placeholder="Select the multifunction to display in the search results",
+    key="multifunction_option"
+)
+
+st.session_state.disabled = False if len(st.session_state.multifunction_option) > 0 else True
+
+left_col, right_col = st.columns(2)
+with left_col:
+    st.selectbox(
+        label="Bionic prototype",
+        options=bionic_prototype_list,
+        help="Select the bionic prototype to display in the search results",
+        placeholder="Select the bionic prototype to display in the search results",
+        index=None,
+        key="bionic_prototype_option",
+        disabled=st.session_state.disabled
+    )
+with right_col:
+    st.multiselect(
+        label="Method",
+        options=method_list,
+        default=[],
+        help="Select the method to display in the search results",
+        placeholder="Select the method to display in the search results",
+        key="method_option",
+        disabled=st.session_state.disabled
+    )
+
+search = st.button("Search")
+if search:
+    multifunction_option = st.session_state.multifunction_option
+    bionic_prototype_option = st.session_state.bionic_prototype_option
+    method_option = st.session_state.method_option
+
+    # Filter the multifunction column
+    filtered_df = df[
+        df["Multifunction"].apply(lambda x: all(multifunction in x for multifunction in multifunction_option))]
+    # Filter the bionic prototype column
+    filtered_df = filtered_df[filtered_df["Bionic prototype"] == bionic_prototype_option] \
+        if (bionic_prototype_option is not None and not st.session_state.disabled and not filtered_df.empty) \
+        else filtered_df
+    # Filter the method column
+    filtered_df = filtered_df[filtered_df["Method"].apply(lambda x: any(method in x for method in method_option))] \
+        if (len(method_option) > 0 and not st.session_state.disabled and not filtered_df.empty) \
+        else filtered_df
+    # Reset the index
+    filtered_df = filtered_df.reset_index(drop=True)
+
+    st.dataframe(
+        filtered_df[st.session_state.display_columns].head(st.session_state.limit),
+        column_config={
+            "Res link": st.column_config.LinkColumn(
+                "Resource", display_text=None
+            )
+        }
+    )
